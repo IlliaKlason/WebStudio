0a9f90bf75d1f1e7fe66b4430fe3bffe328bc3af
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-07-02 21:55:37 +0300

    Summary

---
+++ js/mobile-menu.js
@@ -0,0 +1,37 @@
+// New Repeta's Script
+(() => {
+  const mobileMenu = document.querySelector('.menu-burger');
+  const openMenuBtn = document.querySelector('.menu-burger-btn');
+  const body = document.querySelector('body');
+
+
+
+  
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+    
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
+
+
