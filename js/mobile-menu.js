c715ad5e727594524ae660e22a55162bb1d3cf43
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-07-05 15:12:53 +0300

    fix photo

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,7 +9,7 @@
   
   const toggleMenu = () => {
     const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
     openMenuBtn.classList.toggle('is-open');
@@ -28,9 +28,10 @@
   // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
+
     mobileMenu.classList.remove('is-open');
+    openMenuBtn.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    
   });
 })();
 
